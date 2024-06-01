e6af1f0d2e98d7e8c900d0ef91c771bfe87a09ec
codeirawan <ade_irawan@bridgenote.id>
2024-06-01 22:11:50 +0700

    init

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'vercel_blob_rw_fUC55CHU7Ffbuv42_EQylKmo9hKIGnDfgzXH2Zam6xcQs7P.public.blob.vercel-storage.com',
+      'fuc55chu7ffbuv42.public.blob.vercel-storage.com',
+    ],
+  },
+};
+
+module.exports = nextConfig;
